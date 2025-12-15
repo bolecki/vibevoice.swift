ffb16ea2fc51f0251fa7f4fbfda656b503ebbc6d
Brian Olecki <bolecki019@gmail.com>
2025-12-15 15:55:43 -0500

    add ios platform

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "VibeVoice",
-    platforms: [.macOS(.v14)],
+    platforms: [.macOS(.v14), .iOS(.v16)],
     products: [
         .library(name: "VibeVoice", targets: ["VibeVoice"]),
         .executable(name: "vibevoiceCLI", targets: ["VibeVoiceCLI"])
